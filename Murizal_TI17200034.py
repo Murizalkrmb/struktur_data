c89ce27822a505285bfb4c936e0c8dba9201c42f
Murizalkrmb <80881661+Murizalkrmb@users.noreply.github.com>
2021-03-20 10:06:08 +0700

    Add files via upload

---
+++ Murizal_TI17200034.py
@@ -0,0 +1,14 @@
+print("Hari masuk kuliah:")
+
+Day = ["SENIN","SELASA", "RABU", "KAMIS", "JUM'AT", "SABTU", "AHAD"]
+for i in range(len(Day)):
+    print(i + 1,Day[i])
+    if i== 4: 
+        	print("Hari libur kuliah:")
+    elif i == 6:
+        
+        print("Boleh Seperti ini pak?")
+        
+        
+        
+        
